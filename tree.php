28c6fe7958ba53f96168031c37844c0c23495581
Sascha Ludwig <sl@dienes.de>
2015-06-19 17:41:47 +0200

    added rssi values

--- tree.php
+++ tree.php
@@ -1,69 +1,90 @@
 <?php
   /*
-  * Copyright (c) 2015 Sascha Ludwig / dienes.de
+  * Copyright (c) 2015 Sascha.Ludwig@dienes.de
   * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
   */
 
   error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+  // helper fucntions
   $unhex   = function($value) { return substr($value, 2); };
   $hex2int = function($value) { return hexdec($value); };
 
-  $basestation_settings = file("http://admin:admin@172.23.2.2/Settings.cfg");
-
-  // get all settings values
-  foreach ( $basestation_settings as $line ) {
-    list($key, $value) = split(":", $line);
-    $settings[$key] = $value;
-  }
+  $multicell = join(" ", file("http://admin:admin@172.23.2.1/MultiCell.html"));
 
   // get chain IPs
-  $chain_ip = array_map('trim', split(",", $settings["%NETWORK_SYNC_STATIC_IP_CHAIN%"]));
+  preg_match("/.*SetSyncIpChain\(\"(.*)\"\);.*/", $multicell, $data);
+  $chain_ip = array_map('trim', split(",", $data[1]));
+
   // get chain MACs
-  $chain_mac_tmp = array_map($unhex, split(",", $settings["%NETWORK_SYNC_MAC_CHAIN%"]));
+  preg_match("/.*SetSyncMacChain\(\"(.*)\"\);.*/", $multicell, $data);
+  $chain_mac_tmp = array_map($unhex, split(",", $data[1]));
   for( $i=0 ; $i<sizeof($chain_mac_tmp) ; $i+=6 ) {
     $chain_mac[] = $chain_mac_tmp[$i].":".$chain_mac_tmp[$i+1].":".$chain_mac_tmp[$i+2].":".$chain_mac_tmp[$i+3].":".$chain_mac_tmp[$i+4].":".$chain_mac_tmp[$i+5];
   }
 
   // get chain sync tree
-  $chain_sync = array_map($hex2int, split(",", $settings["%NETWORK_DECT_SYNC_TREE%"]));
+  preg_match("/.*SetSyncTree\(\"(.*)\"\);.*/", $multicell, $data);
+  $chain_sync = array_map($hex2int, split(",", $data[1]));
 
-  // get sync master
-  for( $i=0 ; $i<sizeof($chain_ip); $i++ ) {
-    if( $chain_ip[$i] != '0.0.0.0' ) {
-      if( $i == $chain_sync[$i] ) {
-        $sync_master = $i;
+ // get chain rssi values
+  preg_match("/.*SetSyncDectRssiChain\(\"(.*)\"\);.*/", $multicell, $data);
+  $rssi_tmp = split(":", $data[1]);
+  for ( $idx=0; $idx<sizeof($rssi_tmp) ; $idx++ ) {
+    if ( $rssi_tmp[$idx] != "0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0" ) {
+      $tmp = split(",", $rssi_tmp[$idx]);
+      for ( $rpnidx=0; $rpnidx<sizeof($tmp); $rpnidx++ ) {
+        list( $rpn, $rssi ) = split("\.", $tmp[$rpnidx]);
+        if ( $rssi != 0 ) {
+          $chain_rssi[$idx][$rpn] = round(2.127*$rssi-147.39);
+        }
       }
     }
   }
 
+  // get basestation names
+  preg_match("/.*SetSyncBaseNameChain\(\"(.*)\"\);.*/", $multicell, $data);
+  $bs_names = array_map('trim', split(",", $data[1]));
+
   $graph = sprintf('digraph {
-  rankdir=TB;
-  size="16,9";
-  node [shape = ellipse]; DECT%02d;
+  rankdir=LR;
+  size = "16,9";
+  center=true;
+  ratio=compress;
+  overlap=scale;
+  splines=true;
   node [shape = ellipse];
-  ', $sync_master+1);
+  ');
 
   for( $i=0 ; $i<sizeof($chain_ip); $i++ ) {
       if( $chain_ip[$i] != '0.0.0.0' ) {
           if( $i != $chain_sync[$i] ) {
-              $graph .= sprintf("  DECT%02d->DECT%02d;\n", $chain_sync[$i]+1, $i+1);
+              $rssi = $chain_rssi[$i][$chain_sync[$i]*4];
+              if( $rssi == 0 ) { $rssidb = "unknown"; } else { $rssidb = $rssi."dBm"; }
+              $graph .= sprintf("  %s->%s [label=<<font point-size=\"10\">%s</font>>];\n", $bs_names[$chain_sync[$i]], $bs_names[$i], $rssidb );
           }
           if( $i == $chain_sync[$i] ) {
-              $master_label = "<br/><font point-size=\"8\">MASTER</font>";
+              $master_label = " (MASTER)";
           } else { $master_label = ""; }
-          $graph .= sprintf("  DECT%02d [label=<DECT%02d<br/><font point-size=\"8\">%s</font><br/><font point-size=\"8\">%s</font>%s>];\n", $i+1, $i+1, $chain_mac[$i], $chain_ip[$i], $master_label);
+          $graph .= sprintf("  %s [label=<%s<br/><font point-size=\"8\">%s</font><br/><font point-size=\"8\">%s</font><br/><font point-size=\"8\">RPN%02X%s</font>>];\n", $bs_names[$i], $bs_names[$i], $chain_mac[$i], $chain_ip[$i], $i*4, $master_label);
       }
   }
 
-  $graph .= sprintf('  overlap=false;
-    label="DECT Sync Tree DIENES - %s"
+  $graph .= sprintf('label="DECT Sync Tree DIENES - %s"
     fontsize=12;
   }', date("D M j G:i:s T Y"));
 
   if($_REQUEST['test'] == '1') {
-    echo "<pre>".$graph."</pre>";
+    header("Content-type: text");
+    echo $graph."\n";
+    var_dump($rssi_tmp);
+    var_dump($chain_rssi);
+
   } else {
     header("Content-type: image/png");
-    system("echo ".escapeshellarg($graph)." | neato -Tpng");
+    //system("echo ".escapeshellarg($graph)." | fdp -Tpng");
+    system("echo ".escapeshellarg($graph)." | dot -Tpng");
+    //system("echo ".escapeshellarg($graph)." | twopi -Tpng");
+    //system("echo ".escapeshellarg($graph)." | neato -Tpng");
   }
 ?>
